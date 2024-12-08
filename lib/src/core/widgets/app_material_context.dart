87c4f1babec65d50c5d5dd5b86f4569b54be3ad1
Xurshid <thebestxurshidjon@gmail.com>
2024-12-08 23:05:35 +0500

    connecting olib tawlandi

--- lib/src/core/widgets/app_material_context.dart
+++ lib/src/core/widgets/app_material_context.dart
@@ -1,23 +1,17 @@
-import "package:education_center/src/core/setting/connetion_controller.dart";
-import "package:education_center/src/core/setting/inherted_connection_notifair.dart";
 import "package:flutter/material.dart";
 import "../routes/router_config.dart";
 
-final ConnectionController connetionController = ConnectionController();
 
 class AppMaterialContext extends StatelessWidget {
   const AppMaterialContext({super.key});
 
   @override
-  Widget build(BuildContext context) => InhertedConnectionNotifair(
-        connetionController: connetionController,
-        child: Builder(
-          builder: (context) {
-            return MaterialApp.router(
-              debugShowCheckedModeBanner: false,
-              routerConfig: RouterConfigService.router,
-            );
-          },
-        ),
+  Widget build(BuildContext context) => Builder(
+        builder: (context) {
+          return MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            routerConfig: RouterConfigService.router,
+          );
+        },
       );
 }
